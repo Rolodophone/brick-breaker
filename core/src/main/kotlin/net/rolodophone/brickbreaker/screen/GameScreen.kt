065817bbdc1e65f5dffae5c5fb30ec8a0318dc37
Sergi Lange-Soler <sergi.lange123@gmail.com>
2021-02-19 12:31:00 +0000

    add viewport

--- core/src/main/kotlin/net/rolodophone/brickbreaker/screen/GameScreen.kt
+++ core/src/main/kotlin/net/rolodophone/brickbreaker/screen/GameScreen.kt
@@ -3,6 +3,7 @@ package net.rolodophone.brickbreaker.screen
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.Sprite
+import com.badlogic.gdx.utils.viewport.FitViewport
 import ktx.graphics.use
 import ktx.log.debug
 import ktx.log.logger
@@ -11,6 +12,7 @@ import net.rolodophone.brickbreaker.BrickBreaker
 private val LOG = logger<GameScreen>()
 
 class GameScreen(game: BrickBreaker): BrickBreakerScreen(game) {
+	private val viewport = FitViewport(9 * 32f, 16 * 32f) //288x512
 	private val texture = Texture(Gdx.files.internal("graphics/paddle_normal.png"))
 	private val sprite = Sprite(texture)
 
@@ -19,8 +21,13 @@ class GameScreen(game: BrickBreaker): BrickBreakerScreen(game) {
 		sprite.setPosition(1f, 1f)
 	}
 
+	override fun resize(width: Int, height: Int) {
+		viewport.update(width, height, true)
+	}
+
 	override fun render(delta: Float) {
-		batch.use {
+		viewport.apply()
+		batch.use(viewport.camera.combined) {
 			sprite.draw(it)
 		}
 	}
--- lwjgl3/src/main/kotlin/net/rolodophone/brickbreaker/lwjgl3/Lwjgl3Launcher.kt
+++ lwjgl3/src/main/kotlin/net/rolodophone/brickbreaker/lwjgl3/Lwjgl3Launcher.kt
@@ -8,7 +8,7 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
 fun main() {
 	Lwjgl3Application(BrickBreaker(), Lwjgl3ApplicationConfiguration().apply {
 		setTitle("BrickBreaker")
-		setWindowedMode(640, 480)
+		setWindowedMode(9 * 64, 16 * 64)
 		setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png")
 	})
 }
